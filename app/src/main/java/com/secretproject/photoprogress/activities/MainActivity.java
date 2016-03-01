5934403d25a1612f35e90d27e371e1db0324927d
KimiBitanga <kimibitanga@yahoo.com>
2016-03-01 21:26:26 +0100

    Initial commit

---
+++ app/src/main/java/com/secretproject/photoprogress/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.secretproject.photoprogress.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.secretproject.photoprogress.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
