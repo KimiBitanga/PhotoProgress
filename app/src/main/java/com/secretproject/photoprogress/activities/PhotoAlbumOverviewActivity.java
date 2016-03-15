fd57f6a1a9e9a574fda23d00e1799d5c16d5b9e3
Borislav <borislav.mesi@gmail.com>
2016-03-15 02:06:56 -0700

    Rest of files.

---
+++ app/src/main/java/com/secretproject/photoprogress/activities/PhotoAlbumOverviewActivity.java
@@ -0,0 +1,90 @@
+package com.secretproject.photoprogress.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.secretproject.photoprogress.R;
+import com.secretproject.photoprogress.activities.TakePhotoActivity;
+import com.secretproject.photoprogress.data.PhotoAlbum;
+import com.secretproject.photoprogress.helpers.PhotoAlbumHelper;
+
+import org.w3c.dom.Text;
+
+public class PhotoAlbumOverviewActivity extends AppCompatActivity {
+
+    private PhotoAlbum photoAlbum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_album_overview);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        PhotoAlbum photoAlbum=null;
+        if(photoAlbum!=null) {
+            Toast.makeText(this, photoAlbum.getName(), Toast.LENGTH_SHORT).show();
+        }
+
+        onShowVideoPreviewButtonListener();
+        onMakeImagePostersButtonListener();
+        onMakeNewPhotoButtonListener();
+
+        photoAlbum = PhotoAlbumHelper.CurrentPhotoAlbum;
+        if(photoAlbum == null){
+            //Every activity should have id of photoAlbum in case that android delete static variable to get instance from the file.
+        }
+        setAlbumDataToLayout();
+    }
+
+    private void setAlbumDataToLayout() {
+        if(photoAlbum!=null) {
+            TextView albumNameTV = (TextView) findViewById(R.id.albumNameTextView);
+            albumNameTV.setText(photoAlbum.getName());
+
+            TextView albumDescriptionTV = (TextView) findViewById(R.id.albumDescriptionTextView);
+            albumDescriptionTV.setText(photoAlbum.getName());
+        }
+    }
+
+    public void onShowVideoPreviewButtonListener (){
+        Button showVideoPreviewBtn=(Button)findViewById(R.id.showVideoPreviewBtn);
+        showVideoPreviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void onMakeImagePostersButtonListener (){
+        Button makeImagePostersBtn=(Button)findViewById(R.id.makeImagePostersBtn);
+        makeImagePostersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void onMakeNewPhotoButtonListener(){
+
+        PhotoAlbumHelper.CurrentPhotoAlbum = photoAlbum;
+
+        Button makenewPhotoBtn=(Button)findViewById(R.id.makeNewPhotoBtn);
+        makenewPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PhotoAlbumOverviewActivity.this, TakePhotoActivity.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/secretproject/photoprogress/helpers/PhotoAlbumHelper.java
@@ -0,0 +1,12 @@
+package com.secretproject.photoprogress.helpers;
+
+import com.secretproject.photoprogress.data.PhotoAlbum;
+
+/**
+ * Created by BO on 14-Mar-16.
+ */
+public class PhotoAlbumHelper {
+
+    public static PhotoAlbum CurrentPhotoAlbum;
+
+}
